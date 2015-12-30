43e20f0c833d109078783232e42983e4471bcc9d
RTougjas <raintoug@ut.ee>
2015-12-30 17:25:18 +0200

    updated one view

--- user_guide/views.sql
+++ user_guide/views.sql
@@ -1,9 +1,11 @@
 CREATE VIEW v_menu_items AS
-SELECT menuItem.ID AS menuItem_id, menuItem.name AS menuItem_name, price AS menuItem_price, category.ID AS category_id, category.name AS category_name, location.ID AS location_id, location.name AS location_name FROM
+SELECT menuItem.ID AS menuItem_id, menuItem.name AS menuItem_name, price AS menuItem_price, category.ID AS category_id, category.name AS category_name, generalCategory.id AS general_id, generalCategory.name AS general_name, location.ID AS location_id, location.name AS location_name FROM
 menuItem INNER JOIN
 category ON menuItem.category = category.ID
 INNER JOIN location
-ON menuItem.location = location.ID;
+ON menuItem.location = location.ID
+INNER JOIN generalCategory
+ON category.general = generalCategory.ID;
 
 CREATE VIEW v_categories AS 
 SELECT category.ID AS category_id, category.name AS category_name, category.location AS location_id, location.name AS location_name, generalCategory.ID AS general_id, generalCategory.name AS general_name
